c1bbe30dd68b0a1c31dcb87d3170036657847720
Vincent Ramdhanie <vramdhanie@gmail.com>
2014-12-27 19:38:03 -0400

    Add a second controller to explore

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 angular.module('multiApp', [])
-    .controller('MultiplicationController', function($scope, $attrs){
-    
+    .controller('MultiplicationController', function($scope, $attrs, $rootScope){
+
         $scope.populateNumbers = function(x){
             var numbers = [];
             for(var i = 0; i < x; i++){
@@ -8,15 +8,39 @@ angular.module('multiApp', [])
             }
             return numbers;
         };
-    
+
         $scope.compute = function(a, b){
             return a * b;
         };
-    
+
         $scope.$watch('numberLimit', function(limit){
             $scope.numbers = $scope.populateNumbers(limit);
         });
-    
+
         $scope.numberLimit = $attrs.initialNumberLimit || 10;
-        
+
+        var activeFactorA, activeFactorB;
+        $scope.setActiveFactors = function(a, b) {
+          activeFactorA = a;
+          activeFactorB = b;
+        };
+
+        $scope.matchesFactor = function (a, b) {
+          return a === activeFactorA || b === activeFactorB;
+        };
+
+        $scope.clearActiveFactors = function(){
+          activeFactorA = null;
+          activeFactorB = null;
+        }
+
+        $scope.setActiveNumber = function(number){
+            $rootScope.$broadcast('display', number);
+        }
+
+    })
+    .controller('DisplayController', function($scope){
+       $scope.$on('display', function(event, data){
+         $scope.content = data;
+       });
     });
